22775a9558cfa2d50bf94b31d19f7c6dc0428cfb
erka <erik.westerberg@hotmail.se>
2019-10-08 19:05:19 +0200

    creates a function that for each row creates a new array, imports this in tetris and pass in in as props inside of stage component

---
+++ src/gameHelpers.js
@@ -0,0 +1,9 @@
+export const STAGE_WIDTH = 12;
+export const STATE_HEIGHT = 20;
+
+export const createStage = () => {
+  Array.from(Array(STATE_HEIGHT), () =>
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
+  )
+}
+
