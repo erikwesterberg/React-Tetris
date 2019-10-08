ba4b55ea7e427bcd67d6eeb327341d1cbbaf76bf
erka <erik.westerberg@hotmail.se>
2019-10-08 18:56:59 +0200

    Implemented starting code base in new components

---
+++ src/components/Cell.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Cell = props => (
+  <div>
+    Cell
+  </div>
+)
+
+export default Cell;
---
+++ src/components/StartButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const StartButton = ({callback}) => (
+  <div>
+    Cell
+  </div>
+)
+
+export default StartButton;
